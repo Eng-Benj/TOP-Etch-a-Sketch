723598641454b69bbac49dfb308d885467c7ca55
Eng-Benj <119880823+Eng-Benj@users.noreply.github.com>
2023-01-12 16:56:26 +0000

    Edit js to include two buttons which clear the grid and replace the grid with a new one of user-specified resolution respectively. Edit html and css to center the grid and buttons on the page

--- JavaScript.js
+++ JavaScript.js
@@ -1,25 +1,60 @@
-const topLevel = document.querySelector('.grid')
-for (let i=0;i<16;i++){
-    let divRow = document.createElement('div');
-    divRow.className = "row";
-    topLevel.appendChild(divRow);
+function createRows(res) {
+    const topLevel = document.querySelector('.grid');
+    for (let i=0;i<res;i++){
+        let divRow = document.createElement('div');
+        divRow.className = "row";
+        topLevel.appendChild(divRow);
+    };
+};
+
+function createSquares(res) {
+    const rows = Array.from(document.querySelectorAll('.row'));
+    rows.forEach((row) => {
+        for (let i=0;i<res;i++) {
+            let divSqr = document.createElement('div');
+            divSqr.className = "sqr";
+            divSqr.id = "r" + (rows.indexOf(row)+1) + "c" + (i+1);
+            row.appendChild(divSqr);
+        }
+    });
+};
+
+function pencil() {
+    const sqrs = Array.from(document.querySelectorAll('.sqr'));
+    sqrs.forEach((sqr) => {
+        sqr.addEventListener('mouseover', () => {
+            sqr.style.cssText = "background-color: grey;";
+        });
+    });
+};
+
+function deleteGrid() {
+    const grid = document.querySelector('.grid')
+    const rows = Array.from(document.querySelectorAll('.row'));
+    const sqrs = Array.from(document.querySelectorAll('.sqr'));
+    grid.style.cssText = "border-left: 0px solid black; border-top: 0px solid black"
+    rows.forEach((row) => {
+        grid.removeChild(row)
+    })
 }
 
-const rows = Array.from(document.querySelectorAll('.row'));
-console.log(rows)
-rows.forEach((row) => {
-    for (let i=0;i<16;i++) {
-        let divSqr = document.createElement('div');
-        divSqr.className = "sqr" 
-        divSqr.id = "r" + (rows.indexOf(row)+1) + "c" + (i+1);
-        row.appendChild(divSqr)
+function etchASketch(res){
+    if (res != null && res < 65 && res > 0){
+        deleteGrid()
+        createRows(res)
+        createSquares(res)
+        pencil()
     }
-});
+    console.log(res)
+}
 
-const sqrs = Array.from(document.querySelectorAll('.sqr'));
-console.log(sqrs)
-sqrs.forEach((sqr) => {
-    sqr.addEventListener('mouseover', () => {
-        sqr.style.cssText = "background-color: grey;"
+const newGrid = document.querySelector('#newGrid')
+newGrid.addEventListener('click', () => etchASketch(prompt("How many squares wide would you like the grid to be? (1-64)")))
+
+const clearGrid = document.querySelector('#clearGrid')
+clearGrid.addEventListener('click', () => {
+    const sqrs = Array.from(document.querySelectorAll('.sqr'));
+    sqrs.forEach((sqr) => {
+        sqr.style.cssText = "background-color: white;";
     });
-});
+})
